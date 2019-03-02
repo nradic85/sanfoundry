7060894f622aebd28b422da21bf9120f6294a381
Nikola Radić <nradic85@gmail.com>
2019-03-02 13:45:21 +0100

    Swaps two number

---
+++ sanfoundryExamples/2_ProgramToSwap2Numbers/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_ProgramToSwap2Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp = 0, firstNumber = 0, secondNumber = 0;
+            Console.WriteLine("Insert first number: ");
+            firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insert second number: ");
+            secondNumber = int.Parse(Console.ReadLine());
+            temp = firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = temp;
+            Console.WriteLine("After swaps first number is: "+firstNumber+" and second number is: "+secondNumber);
+            Console.ReadKey();
+        }
+    }
+}
